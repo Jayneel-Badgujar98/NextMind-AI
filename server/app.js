6ffb2fdc9b89d6b4d00b2d5f682fd6d1ef890cad
Jayneel Badgujar <jayneelbadgujar@gmail.com>
2026-06-24 16:52:33 +0530

    Cors issue solving

--- server/app.js
+++ server/app.js
@@ -12,7 +12,8 @@ const app = express() ;
 const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "http://127.0.0.1:5173"
+  "http://127.0.0.1:5173",
+  "https://nextmindai.vercel.app"
 ];
 
 if (process.env.CLIENT_URL) {
